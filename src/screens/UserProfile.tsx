052238f61ddac9b3c032bcacfa51d177f8d9f075
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-11-19 18:15:05 -0600

    Use hooks by router

--- src/screens/UserProfile.tsx
+++ src/screens/UserProfile.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { fetchUser } from '../actions';
 
-export default (props: any) => {
+export default () => {
+  const dispatch = useDispatch();
   const { username } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchUser(username || ''));
+  }, [dispatch, username]);
+
   return <div>{username}</div>;
 };
